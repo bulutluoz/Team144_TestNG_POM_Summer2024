57ff3c5b76c25fe2f04b65b4a87bdc7b28776726
bulutluoz <abbulutluoz@gmail.com>
2024-07-31 09:45:43 +0200

    day14 sonu

--- src/test/java/pages/TestOtomasyonuPage.java
+++ src/test/java/pages/TestOtomasyonuPage.java
@@ -2,6 +2,8 @@ package pages;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
 
 import java.util.List;
 
@@ -14,6 +16,10 @@ public class TestOtomasyonuPage {
         Page class'lari TestNG tarafindan ozel tasarlanmistir
         ve locate yaparken @FindBy kullanilir
      */
+
+    public TestOtomasyonuPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
 
     // arama kutusu
     @FindBy(id = "global-search")
--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -2,6 +2,7 @@ package utilities;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 
@@ -12,9 +13,14 @@ public class Driver {
     public static WebDriver driver;
 
     public static WebDriver getDriver(){
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        if (driver == null){
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--disable-search-engine-choice-screen");
+            driver = new ChromeDriver(options);
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        }
 
         return driver;
     }
